2d62999ee71be6f70f81dbca50c280bf73521f2e
Ryosuke Hamaji <aibou7251@gmail.com>
2021-10-07 17:16:11 +0900

    Update iam.tf

--- iam.tf
+++ iam.tf
@@ -13,7 +13,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
     # MEMO: リポジトリ名だけで制御したい場合はこちら
     condition {
       test = "StringLike"
-      variable = "vstoken.actions.githubusercontent.com:aud"
+      variable = "token.actions.githubusercontent.com:aud"
       values = [
         "https://github.com/${local.reponame}"
       ]
@@ -22,7 +22,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
     # MEMO: ブランチで制御したい場合はこちら（ブランチ名がmasterではない場合、CIは走るがAssumeRoleWithWebIdentityに失敗してエラーになる）
     # condition {
     #   test = "StringLike"
-    #   variable = "vstoken.actions.githubusercontent.com:sub"
+    #   variable = "token.actions.githubusercontent.com:sub"
     #   values = [
     #     "repo:${local.reponame}:ref:refs/heads/master"
     #   ]
@@ -32,7 +32,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
 
 # 同一AWSアカウント内に複数のこのURLでのIDプロバイダを作成できないため、このIDプロバイダを共有することになる
 resource "aws_iam_openid_connect_provider" "github" {
-  url = "https://vstoken.actions.githubusercontent.com"
+  url = "https://token.actions.githubusercontent.com"
   thumbprint_list = ["a031c46782e6e6c662c2c87c76da9aa62ccabd8e"]
   client_id_list = ["https://github.com/${local.reponame}"]
 }
